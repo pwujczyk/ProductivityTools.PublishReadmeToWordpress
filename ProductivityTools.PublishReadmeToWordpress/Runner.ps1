26e44dd0b35179ef70866e3d1af0d00e214f7c7d
Pawel Wujczyk <pwujczyk@hotmail.com>
2022-03-29 07:04:03 +0200

    readme

--- ProductivityTools.PublishReadmeToWordpress/Runner.ps1
+++ ProductivityTools.PublishReadmeToWordpress/Runner.ps1
@@ -13,5 +13,5 @@ Clear-Host
 
 #Publish-ReadmesToBlog $ReadmeMasterDirectory -DestinationTempPath $DestinationTempPath -CdnImageAddress $CdnImageAddress -PushImagesToAzure $true -verbose
 Import-Module ProductivityTools.ConvertDocuments
-$Directory="d:\GitHub\ProductivityTools.ConnectionStringLight\"
+$Directory="d:\GitHub\productivitytools.learning.reactwithfirebaseauthwithdb\"
 Publish-ReadmeToBlog -Directory $Directory -DestinationTempPath $DestinationTempPath -CdnImageAddress $CdnImageAddress -PushImagesToAzure $true -verbose
